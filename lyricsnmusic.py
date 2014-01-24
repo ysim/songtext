9efbbe487ce27690570001e0e9f50382ac587aa0
ysim <ysim90@gmail.com>
2014-01-24 16:48:59 -0500

    be able to search by artist too

--- lyricsnmusic.py
+++ lyricsnmusic.py
@@ -16,6 +16,12 @@ except KeyError:
     raise
 CSS_SELECTOR = "pre[itemprop='description']"
 
+API_PARAMETERS = {
+    'query': 'q',
+    'artist': 'artist',
+    'title': 'track'
+}
+
 
 class Track(object):
 
@@ -40,13 +46,16 @@ class Track(object):
 
 class TrackList(object):
 
-    def __init__(self, query=None):
-        self.query = query
-        self.response = self.get_response(query)
+    def __init__(self, args):
+        self.args = args
+        self.response = self.get_response(args)
         self.json = self.response.json()
 
-    def get_response(self, query=None):
-        params = { 'api_key': API_KEY, 'q': query }
+    def get_response(self, args):
+        params = { 'api_key': API_KEY }
+        for arg in ('query', 'artist'):
+            if args[arg] is not None:
+                params[API_PARAMETERS[arg]] = args[arg]
         response = requests.get(API_URL, params=params)
         return response
 
@@ -89,35 +98,35 @@ class TrackList(object):
         return output
 
 
-def check_matches(tracklist_object, query):
+def check_matches(tracklist_object):
     count = tracklist_object.count
     if count == 0:
         print "\nNo tracks matching your query were found.\n\n"
         return False
-    print '\n{0} track(s) matched your search query "{1}".\n\n'.format(count, query)
+    print '\n{0} track(s) matched your search query.\n\n'.format(count)
     return True
 
 
-def get_track_list(query, limit):
-    track_list = TrackList(query)
-    num_matches = check_matches(track_list, query)
+def get_track_list(args):
+    track_list = TrackList(args)
+    num_matches = check_matches(track_list)
     if not num_matches:
         return 1
-    print "Displaying top {0} matches (out of {1}):".format(limit, num_matches)
-    print track_list.get_list(limit)
+    print "Displaying top {0} matches (out of {1}):\n".format(args['limit'], num_matches)
+    print track_list.get_list(args['limit'])
     return 0
 
 
-def get_track(query, index):
-    track_list = TrackList(query)
-    num_matches = check_matches(track_list, query)
+def get_track(args):
+    track_list = TrackList(args)
+    num_matches = check_matches(track_list)
     if not num_matches:
         return 1
-    if not track_list.is_track_viewable(index):
+    if not track_list.is_track_viewable(args['index']):
         print "The requested track is not viewable."
         print
         return 1
-    track = Track(track_list.get_track_url(index))
-    print track_list.get_info(int(index))
+    track = Track(track_list.get_track_url(args['index']))
+    print track_list.get_info(int(args['index']))
     print track.get_lyrics()
     return 0
