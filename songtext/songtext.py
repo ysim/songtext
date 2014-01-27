82e0049c924703f817c60d92f197b10495ff2120
ysim <ysim90@gmail.com>
2014-01-27 16:19:06 -0500

    amending some of the help text for the parser

--- songtext/songtext.py
+++ songtext/songtext.py
@@ -29,12 +29,15 @@ def get_song_lyrics(args):
 def get_parser():
     parser = argparse.ArgumentParser(description='grab some song lyrics')
     parser.add_argument('query', metavar='QUERY', type=str, nargs='*',
-        help='search all fields (artist name, song title, words in lyrics), '
+        help='Search all fields (artist name, song title, words in lyrics), '
         'e.g. "daft punk get lucky"')
-    parser.add_argument('-l', '--list', metavar='NUM_MATCHES', type=int,
-        const=10, nargs='?', dest='limit', help='list the first n matches '
-        'along with a snippet from the lyrics')
-    parser.add_argument('-i', '--index', dest='index', type=int, default=0)
+    parser.add_argument('-l', '--list', metavar='N', type=int,
+        const=10, nargs='?', dest='limit', help='List the top N matches for '
+        'your search query along with a snippet from the lyrics if possible '
+        '(defaults to 10)')
+    parser.add_argument('-i', '--index', metavar='INDEX', type=int, default=0,
+        help='Display the lyrics for match number INDEX (from the list when '
+        'running the same search query with the --list option)')
     parser.add_argument('-a', '--artist', metavar='ARTIST_NAME', type=str,
         nargs='+')
     parser.add_argument('-t', '--title', metavar='SONG_TITLE', type=str,
