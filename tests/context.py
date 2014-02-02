e13d85eda54673841688e9dcb471206f124f6b4d
ysim <opensource@yiqingsim.net>
2014-02-01 22:43:21 -0500

    reorganizing tests

---
+++ tests/context.py
@@ -0,0 +1,9 @@
+import os
+import sys
+
+# Temporarily add the repo root to your $PATH
+FILE_BASE_PATH = os.path.abspath(os.path.dirname(__file__))
+ROOT_PATH = os.path.abspath(os.path.dirname(FILE_BASE_PATH))
+sys.path.insert(0, ROOT_PATH)
+
+from libsongtext import songtext
