83b239e65bd9a1d2c35845c183e5286f0a26780c
Yi Qing Sim <tubbslechat@fastmail.net>
2014-02-04 11:09:24 -0500

    changing 'return_value' to 'ret_value' in tests so that vim syntax plugins won't freak out

--- tests/test_songtext.py
+++ tests/test_songtext.py
@@ -15,33 +15,44 @@ class LyricsNMusicTests(unittest.TestCase):
 
     def test_get_single_result_with_artist_and_song_title(self):
         arg_dict = self.get_args('-a johnny flynn -t the box')
-        return_value = songtext.get_song_lyrics(arg_dict)
-        self.assertEqual(return_value, 0)
+        ret_value = songtext.get_song_lyrics(arg_dict)
+        self.assertEqual(ret_value, 0)
 
     def test_get_single_result_with_artist_only(self):
         arg_dict = self.get_args('-a laura marling')
-        return_value = songtext.get_song_lyrics(arg_dict)
-        self.assertEqual(return_value, 0)
+        ret_value = songtext.get_song_lyrics(arg_dict)
+        self.assertEqual(ret_value, 0)
 
     def test_get_single_result_with_song_title_only(self):
         arg_dict = self.get_args('-t lillian egypt')
-        return_value = songtext.get_song_lyrics(arg_dict)
-        self.assertEqual(return_value, 0)
+        ret_value = songtext.get_song_lyrics(arg_dict)
+        self.assertEqual(ret_value, 0)
+
+    def test_get_single_result_with_words_only(self):
+        arg_dict = self.get_args('-w all the rowboats in the paintings')
+        ret_value = songtext.get_song_lyrics(arg_dict)
+        self.assertEqual(ret_value, 0)
 
     def test_get_list_result_with_artist_and_song_title(self):
         arg_dict = self.get_args('-a the xx -t infinity -l')
-        return_value = songtext.get_song_lyrics(arg_dict)
-        self.assertEqual(return_value, 0)
+        ret_value = songtext.get_song_lyrics(arg_dict)
+        self.assertEqual(ret_value, 0)
 
     def test_get_list_result_with_artist_only(self):
         arg_dict = self.get_args('-a yeah yeah yeahs -l')
-        return_value = songtext.get_song_lyrics(arg_dict)
-        self.assertEqual(return_value, 0)
+        ret_value = songtext.get_song_lyrics(arg_dict)
+        self.assertEqual(ret_value, 0)
 
     def test_get_list_result_with_song_title_only(self):
         arg_dict = self.get_args('-t gimme sympathy')
-        return_value = songtext.get_song_lyrics(arg_dict)
-        self.assertEqual(return_value, 0)
+        ret_value = songtext.get_song_lyrics(arg_dict)
+        self.assertEqual(ret_value, 0)
+
+    def test_get_list_result_with_words_only(self):
+        arg_dict = self.get_args('-w i see a red door and i want it painted '
+            'black -l')
+        ret_value = songtext.get_song_lyrics(arg_dict)
+        self.assertEqual(ret_value, 0)
 
 
 class LyricWikiTests(unittest.TestCase):
@@ -54,28 +65,28 @@ class LyricWikiTests(unittest.TestCase):
 
     def test_get_single_result_with_artist_and_song_title(self):
         arg_dict = self.get_args('-a mumford and sons -t the cave')
-        return_value = songtext.get_song_lyrics(arg_dict)
-        self.assertEqual(return_value, 0)
+        ret_value = songtext.get_song_lyrics(arg_dict)
+        self.assertEqual(ret_value, 0)
 
     def test_searching_with_list_option_fails(self):
         arg_dict = self.get_args('-a josh rouse -l')
-        return_value = songtext.get_song_lyrics(arg_dict)
-        self.assertEqual(return_value, 1)
+        ret_value = songtext.get_song_lyrics(arg_dict)
+        self.assertEqual(ret_value, 1)
 
     def test_searching_with_words_option_fails(self):
         arg_dict = self.get_args('-w home is wherever i\'m with you')
-        return_value = songtext.get_song_lyrics(arg_dict)
-        self.assertEqual(return_value, 1)
+        ret_value = songtext.get_song_lyrics(arg_dict)
+        self.assertEqual(ret_value, 1)
 
     def test_searching_with_artist_only_fails(self):
         arg_dict = self.get_args('-a arcade fire')
-        return_value = songtext.get_song_lyrics(arg_dict)
-        self.assertEqual(return_value, 1)
+        ret_value = songtext.get_song_lyrics(arg_dict)
+        self.assertEqual(ret_value, 1)
 
     def test_searching_with_song_title_only_fails(self):
         arg_dict = self.get_args('-t stairway to heaven')
-        return_value = songtext.get_song_lyrics(arg_dict)
-        self.assertEqual(return_value, 1)
+        ret_value = songtext.get_song_lyrics(arg_dict)
+        self.assertEqual(ret_value, 1)
 
 
 if __name__ == '__main__':
