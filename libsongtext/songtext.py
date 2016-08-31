e8b81c12494aff9498767a37f5054112843c7780
Yi Qing Sim <opensource@yiqingsim.net>
2016-08-31 23:52:51 +0200

    Complete refactor of songtext.py and lyricwiki.py to remove use of abstract base classes (#25), use pager for output (#3), and use click instead of argparse (#29)

--- libsongtext/songtext.py
+++ libsongtext/songtext.py
@@ -3,58 +3,39 @@ from importlib import import_module  # __import__ does not resolve dotted paths
 import os
 import sys
 
+import click
+
 from .properties import __version__
 
 
 DEFAULT_API = os.environ.get('SONGTEXT_DEFAULT_API', 'lyricwiki')
 
 
-def process_args(args):
-    if args.pop('version'):
-        print '%s.%s.%s' % __version__
+@click.command()
+@click.option('--api', default=DEFAULT_API, type=click.Choice(['lyricwiki',
+'lyricsnmusic']))
+@click.option('-V', '--version', is_flag=True)
+@click.option('-a', '--artist')
+@click.option('-t', '--title')
+@click.option('-w', '--words')
+@click.option('-l', '--limit')
+@click.option('-i', '--index')
+@click.argument('all_fields', required=False)
+def cli(api, version, artist, title, words, limit, index, all_fields):
+    if version:
+        click.echo('{}.{}.{}'.format(*__version__))
         return 0
-    get_song_lyrics(args)
 
-
-def get_song_lyrics(args):
-    api = import_module(__package__ + '.' + ''.join(args.pop('api')))
-
-    for arg in getattr(api, 'SEARCH_PARAMETERS').keys():
-        if args[arg] is not None:
-            args[arg] = ' '.join(args[arg])
-
-    return getattr(api, 'get_result')(args)
-
-
-def get_parser():
-    parser = argparse.ArgumentParser(description='a command line song lyric '
-        'fetcher')
-    parser.add_argument('query', metavar='QUERY', type=str, nargs='*',
-        help='Search all fields (artist name, song title, words in lyrics), '
-        'e.g. "daft punk get lucky"')
-    parser.add_argument('-l', '--list', metavar='N', type=int,
-        const=10, nargs='?', dest='limit', help='List the top N matches for '
-        'your search query along with a snippet from the lyrics if possible '
-        '(defaults to 10)')
-    parser.add_argument('-i', '--index', metavar='INDEX', type=int, default=0,
-        help='Display the lyrics for match number INDEX (from the list when '
-        'running the same search query with the --list option)')
-    parser.add_argument('-a', '--artist', metavar='ARTIST_NAME', type=str,
-        nargs='+')
-    parser.add_argument('-t', '--title', metavar='SONG_TITLE', type=str,
-        nargs='+')
-    parser.add_argument('-w', '--words', metavar='LYRICS', type=str,
-        nargs='+')
-    parser.add_argument('--api', metavar='API_MODULE', type=str, nargs=1,
-        default=DEFAULT_API)
-    parser.add_argument('-V', '--version', action='store_true')
-    return parser
+    api_module = import_module(__package__ + '.' + ''.join(api))
+    params = {
+        'artist': artist,
+        'title': title,
+        'words': words,
+        'limit': limit,
+        'index': index
+    }
+    return getattr(api_module, 'get_result')(params)
 
 
 def main():
-    parser = get_parser()
-    if not len(sys.argv) > 1:
-        parser.print_help()
-        return 1
-    args = vars(parser.parse_args())
-    return process_args(args)
+    return cli()
