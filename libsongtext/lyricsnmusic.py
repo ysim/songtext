8b261027559f55b0cb89be74bde74f67f4248004
Yi Qing Sim <opensource@yiqingsim.net>
2016-09-01 17:28:28 +0200

    Refactor lyricsnmusic api module

--- libsongtext/lyricsnmusic.py
+++ libsongtext/lyricsnmusic.py
@@ -1,14 +1,15 @@
 # LYRICSnMUSIC API wrapper
 # Documentation: http://www.lyricsnmusic.com/api
 
+from pydoc import pager
 import os
 
+from lxml import html
 import requests
 
-from base import BaseTrack, BaseTrackList
+from errors import SearchError
 
 
-API_URL = 'http://api.lyricsnmusic.com/songs'
 try:
     API_KEY = os.environ['LYRICSNMUSIC_API_KEY']
 except KeyError:
@@ -19,68 +20,50 @@ except KeyError:
     print "\thttp://www.lyricsnmusic.com/api_keys/new\n"
     raise
 
+
+API_URL = 'http://api.lyricsnmusic.com/songs'
 SEARCH_PARAMETERS = {
-    'query': 'q',
+    'all_fields': 'q',
     'artist': 'artist',
     'title': 'track',
     'words': 'lyrics',
 }
 
 
-class Track(BaseTrack):
+class LNMTrackList(object):
 
     CSS_SELECTOR = "pre[itemprop='description']"
 
-    def get_lyrics(self):
-        print u'{0}\n\n'.format(self.element.text_content())
-        return 0
+    def __init__(self, args):
+        self.args = args.copy()
 
+        if self.args['limit'] is None:
+            self.args['limit'] = 10
 
-class TrackList(BaseTrackList):
+        if self.args['index'] is None:
+            self.args['index'] = 0
 
-    def get_response(self, args):
+        self.response = self.get_response()
+        self.json = self.response.json()
+
+        self.count = len(self.json)
+        if self.count == 0:
+            print("\nNo tracks matching your query were found.\n\n")
+            raise SearchError
+        else:
+            print("\n{} track(s) matched your search query.\n\n".format(self.count))
+
+    def get_response(self):
         params = { 'api_key': API_KEY }
-        for arg in SEARCH_PARAMETERS.keys():
-            if args[arg] is not None:
-                params[SEARCH_PARAMETERS[arg]] = args[arg]
+        for k, v in SEARCH_PARAMETERS.items():
+            if self.args[k] is not None:
+                params[v] = self.args[k]
         response = requests.get(API_URL, params=params)
         return response
 
-    @property
-    def count(self):
-        return len(self.json)
-
-    def is_valid(self):
-        if self.count == 0:
-            print "\nNo tracks matching your query were found.\n\n"
-            return False
-        print '\n{0} track(s) matched your search query.\n\n'.format(self.count)
-        return True
-
-    def get_track_url(self, index=0):
-        return self.json[index]['url']
-
-    def get_info(self, index=0):
-        output = ""
-        line1 = u'{0}: {1}\n'.format(
-            self.json[index]['artist']['name'],
-            self.json[index]['title']
-        )
-        line2 = "{0}\n".format("-" * len(line1))
-        output += line1
-        output += line2
-        return output
-
-    def is_track_viewable(self, index):
-        is_viewable = self.json[index]['viewable']
-        if not is_viewable:
-            print 'The requested track is not viewable.\n'
-            return False
-        return True
-
-    def get_list(self, limit):
-        output = 'Displaying the top {0} matches:\n\n'.format(limit)
-        for index, track in enumerate(self.json[:limit]):
+    def get_list(self):
+        output = 'Displaying the top {0} matches:\n\n'.format(self.args['limit'])
+        for index, track in enumerate(self.json[:self.args['limit']]):
             viewable = track['viewable']
             line = u'{0:>3}. {1}: {2}'.format(
                 index,
@@ -94,18 +77,34 @@ class TrackList(BaseTrackList):
             else:
                 line += u' (full lyrics unavailable)\n'
             output += line
-        print output
-        return 0
+        return output
+
+    def get_info(self):
+        output = ""
+        line1 = u'{0}: {1}\n'.format(
+            self.json[self.args['index']]['artist']['name'],
+            self.json[self.args['index']]['title']
+        )
+        line2 = "{0}\n".format("-" * len(line1))
+        output += line1
+        output += line2
+        return output
+
+    def get_lyrics(self):
+        track_url = self.json[self.args['index']]['url']
+        response = requests.get(track_url)
+        page_html = html.document_fromstring(response.text)
+        element = page_html.cssselect(self.CSS_SELECTOR)[0]
+        return u'{0}\n\n'.format(element.text_content())
 
 
 def get_result(args):
-    tracklist = TrackList(args)
-    if not tracklist.is_valid():
-        return 1
-    if args['limit'] is not None:
-        return tracklist.get_list(args['limit'])
-    if not tracklist.is_track_viewable(args['index']):
-        return 1
-    track = Track(tracklist.get_track_url(args['index']))
-    print tracklist.get_info(int(args['index']))
-    return track.get_lyrics()
+    tracklist = LNMTrackList(args)
+
+    if args['list']:
+        print(tracklist.get_list())
+        if args['index'] is None:
+            return 0
+
+    pager(tracklist.get_info() + tracklist.get_lyrics())
+    return 0
