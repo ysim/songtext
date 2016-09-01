d1b92b222d4b2b5a58c526ee6406fe062de5729f
ysim <opensource@yiqingsim.net>
2016-09-01 17:28:28 +0200

    Refactor lyricsnmusic api module

--- libsongtext/songtext.py
+++ libsongtext/songtext.py
@@ -9,19 +9,20 @@ from .properties import __version__
 
 
 DEFAULT_API = os.environ.get('SONGTEXT_DEFAULT_API', 'lyricwiki')
+AVAILABLE_APIS = ['lyricwiki', 'lyricsnmusic']
 
 
 @click.command()
-@click.option('--api', default=DEFAULT_API, type=click.Choice(['lyricwiki',
-'lyricsnmusic']))
+@click.option('--api', default=DEFAULT_API, type=click.Choice(AVAILABLE_APIS))
 @click.option('-V', '--version', is_flag=True)
 @click.option('-a', '--artist')
 @click.option('-t', '--title')
 @click.option('-w', '--words')
-@click.option('-l', '--limit')
-@click.option('-i', '--index')
-@click.argument('all_fields', required=False)
-def cli(api, version, artist, title, words, limit, index, all_fields):
+@click.option('-l', '--show-list', default=False, is_flag=True)
+@click.option('--limit', default=10)
+@click.option('-i', '--index', type=int)
+@click.argument('all_fields', required=False, nargs=-1)
+def cli(api, version, artist, title, words, show_list, limit, index, all_fields):
     if version:
         click.echo('{}.{}.{}'.format(*__version__))
         return 0
@@ -31,8 +32,10 @@ def cli(api, version, artist, title, words, limit, index, all_fields):
         'artist': artist,
         'title': title,
         'words': words,
+        'list': show_list,
         'limit': limit,
-        'index': index
+        'index': index,
+        'all_fields': all_fields
     }
     return getattr(api_module, 'get_result')(params)
 
