6e8a88e08cd16e2b28ce04322783e1ac0bf9c7df
Yi Qing Sim <opensource@yiqingsim.net>
2017-04-07 17:32:21 +0200

    Bump version number for 0.1.6 -> 0.1.7 in libsongtext/properties.py and tests/test_local_options.py

--- tests/test_local_options.py
+++ tests/test_local_options.py
@@ -16,4 +16,4 @@ class TestGeneral:
     def test_version(self):
         result = runner.invoke(songtext.cli, ['--version'])
         assert result.exit_code == 0
-        assert result.output == '0.1.6\n'
+        assert result.output == '0.1.7\n'
