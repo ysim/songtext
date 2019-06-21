48f93367b2dc4b7606a17755a69e2467829d21a9
ysim <opensource@yiqingsim.net>
2019-06-21 16:21:51 -0400

    Bump version from 0.1.8 -> 0.1.9

--- tests/test_local_options.py
+++ tests/test_local_options.py
@@ -16,4 +16,4 @@ class TestGeneral:
     def test_version(self):
         result = runner.invoke(songtext.cli, ['--version'])
         assert result.exit_code == 0
-        assert result.output == '0.1.8\n'
+        assert result.output == '0.1.9\n'
